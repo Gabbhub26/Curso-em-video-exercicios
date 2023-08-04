4f517cc6448893911e59f396fe3fb92745c63b9c
Gabbhub26 <c3porsd2@gmail.com>
2023-08-03 22:47:30 -0300

    projeto

---
+++ Desafio 42.py
@@ -0,0 +1,8 @@
+print('-//-'*30)
+print('analisador de triangulos')
+print('-//-'*30)
+
+Reta1 = float(input('primeira rata: '))
+Reta2 = float(input('segunda rata: '))
+Reta3 = float(input('terceira rata: '))
+
